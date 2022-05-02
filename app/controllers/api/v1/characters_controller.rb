d8c2e278d0c4be371a0b83cf2f424111796af98b
Alex <alexandreliskiewicz@hotmail.com>
2022-05-02 08:53:41 +0200

    add

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CharactersController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :set_character, only: [ :show, :edit,  :update, :destroy ]
+
   def index
     @characters = Character.all
     render json: @characters
